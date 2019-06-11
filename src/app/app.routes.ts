82441a01f2f9368c5c624e03a9122da2509e3877
Abdul Rehman <49514505+ExaltedSystems@users.noreply.github.com>
2019-06-11 15:39:19 +0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { FlightsListingComponent } from './components/flights-listing/flights-listing.component';
 import { FlightBookingComponent } from './components/flight-booking/flight-booking.component';
 import { PnrViewComponent } from './components/pnr-view/pnr-view.component';
 import { ThankYouComponent } from './components/thank-you/thank-you.component';
-
-export const APP_ROUTES: Routes = [
-  	{ path: '', component: HomeComponent },
-  	{ path: 'flights-listing', component: FlightsListingComponent },
-  	{ path: 'flight-booking', component: FlightBookingComponent },
-  	{ path: 'pnrView', component: PnrViewComponent },
-  	{ path: 'thank-you', component: ThankYouComponent }
-=======
-import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { FlightsListingComponent } from './components/flights-listing/flights-listing.component';
-import { FlightBookingComponent } from './components/flight-booking/flight-booking.component';
-import { PnrViewComponent } from './components/pnr-view/pnr-view.component';
 import { HotelsListingComponent } from 'src/app/components/hotels/hotels-listing/hotels-listing.component';
 import { HotelDetailsComponent } from 'src/app/components/hotels/hotel-details/hotel-details.component';
 import { ContactusComponent } from 'src/app/components/contactus/contactus.component';
@@ -30,8 +16,6 @@ import { DealsComponent } from	'src/app/components/deals/deals.component';
 import { ToursComponent } from	'src/app/components/tours/tours.component';
 import { AboutusComponent } from  'src/app/components/aboutus/aboutus.component';
 import  { DetailComponent } from 'src/app/components/popular-airline-and-hotels/detail/detail.component';
-
-
 
 export const APP_ROUTES: Routes = [
 	{ path: '', component: HomeComponent },
@@ -49,10 +33,10 @@ export const APP_ROUTES: Routes = [
 	{ path: 'contactUs', component: ContactusComponent },
 	{ path: 'flights-listing', component: FlightsListingComponent },
 	{ path: 'flight-booking', component: FlightBookingComponent },
-	{ path: 'pnrView', component: PnrViewComponent },
+  { path: 'pnrView', component: PnrViewComponent },
+  { path: 'thank-you', component: ThankYouComponent }
 	{ path: 'hotels-listing', component: HotelsListingComponent },
 	{ path: 'hotel-details', component: HotelDetailsComponent },
 	{ path: 'airlines/:airline', component : DetailComponent},
 	{ path: 'hotels/:hotel', component : DetailComponent}
->>>>>>> master
 ];
