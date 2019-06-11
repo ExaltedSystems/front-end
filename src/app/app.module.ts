47a23f41c067ad951bb7efdb18125c5c69b179d6
Abdul Rehman <49514505+ExaltedSystems@users.noreply.github.com>
2019-06-11 16:09:43 +0500

    Sync with Master
    Update changes from Master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
@@ -8,7 +7,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 import { APP_ROUTES } from './app.routes';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatDatepickerModule, MatInputModule, MatFormFieldModule, MatRadioModule, MatNativeDateModule, MatAutocompleteModule, MatSelectModule, MatSidenavModule, MatDrawerContainer, MatDrawer, MatSidenav, MatProgressBarModule, MatCheckboxModule, MatExpansionModule, MatStepperModule, MatProgressSpinnerModule, MatCardModule, MatTabsModule} from '@angular/material';
+import {MatDatepickerModule, MatInputModule, MatFormFieldModule, MatRadioModule, MatNativeDateModule, MatAutocompleteModule, 
+  MatSelectModule, MatSidenavModule, MatDrawerContainer, MatDrawer, MatSidenav, MatProgressBarModule, MatCheckboxModule,
+   MatExpansionModule, MatStepperModule, MatProgressSpinnerModule, MatCardModule, MatButtonModule, MatTabsModule} from '@angular/material';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -30,98 +31,6 @@ import { PnrViewComponent } from './components/pnr-view/pnr-view.component';
 import { CookieService } from 'ngx-cookie-service';
 import {StickyModule} from 'ng2-sticky-kit';
 import { ThankYouComponent } from './components/thank-you/thank-you.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    NavbarComponent,
-    FlightsComponent,
-    HotelsComponent,
-    FlightsSearchComponent,
-    HotelsSearchComponent,
-    HomeComponent,
-    FlightsListingComponent,
-    AirPortsPipe,
-    SecondsPipePipe,
-    
-    FlightBookingComponent,
-    
-    DateFormatPipe,
-    
-    PnrViewComponent,
-    
-    ThankYouComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    NoopAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    CommonModule,
-    HttpClientModule,
-    HttpModule,
-    // Material Modules
-    MatInputModule,
-    MatFormFieldModule,
-    MatDatepickerModule,
-    MatRadioModule,
-    MatCheckboxModule,
-    MatNativeDateModule,
-    MatAutocompleteModule,
-    MatSelectModule,
-    IconsModule,
-    MatSidenavModule,
-    MatProgressBarModule,
-    InfiniteScrollModule,
-    MatExpansionModule,
-    MatStepperModule,
-    MatProgressSpinnerModule,
-    MatCardModule,
-    MatSidenavModule,
-    StickyModule,
-    MatTabsModule,
-    RouterModule.forRoot(APP_ROUTES)
-  ],
-  providers: [MainService, CookieService],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
-=======
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpModule } from '@angular/http';
-import { APP_ROUTES } from './app.routes';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatDatepickerModule, MatInputModule, MatFormFieldModule, MatRadioModule, MatNativeDateModule, MatAutocompleteModule, 
-  MatSelectModule, MatSidenavModule, MatDrawerContainer, MatDrawer, MatSidenav, MatProgressBarModule, MatCheckboxModule,
-   MatExpansionModule, MatStepperModule, MatProgressSpinnerModule, MatCardModule, MatButtonModule} from '@angular/material';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FlightsComponent } from './components/flights/flights.component';
-import { HotelsComponent } from './components/hotels/hotels.component';
-import { FlightsSearchComponent } from './components/flights-search/flights-search.component';
-import { HotelsSearchComponent } from './components/hotels-search/hotels-search.component';
-import { HomeComponent } from './components/home/home.component';
-import { IconsModule } from './icons/icons.module';
-import { MainService } from './services/main.service';
-import { FlightsListingComponent } from './components/flights-listing/flights-listing.component';
-import { AirPortsPipe } from './air-ports.pipe';
-import { SecondsPipePipe } from './pipes/seconds-pipe.pipe';
-import { FlightBookingComponent } from './components/flight-booking/flight-booking.component';
-import { DateFormatPipe } from './pipes/date-format.pipe';
-import { PnrViewComponent } from './components/pnr-view/pnr-view.component';
-import { CookieService } from 'ngx-cookie-service';
-import {StickyModule} from 'ng2-sticky-kit';
 import { HotelSearchFormComponent } from './components/hotels/hotel-search-form/hotel-search-form.component';
 import { HotelsListingComponent } from './components/hotels/hotels-listing/hotels-listing.component';
 import { HotelDetailsComponent } from './components/hotels/hotel-details/hotel-details.component';
@@ -157,44 +66,29 @@ import { VisaListsComponent } from './components/visa/visa-lists/visa-lists.comp
     AirPortsPipe,
     SecondsPipePipe,
     FlightBookingComponent,
-    
     DateFormatPipe,
-    
     PnrViewComponent,
-    
+    ThankYouComponent,
     HotelSearchFormComponent,
-    
     HotelsListingComponent,
-    
     HotelDetailsComponent,
-    
     HotelInlineSearchFormComponent,
-    
     HotelFiltersPipe,
-    
-    ContactusComponent,
-    
-    AboutusComponent,
-    
-    ToursComponent,
-    
-    DealsComponent,
-    
-    UmrahComponent,
-    
-    VisaComponent,
-    
-    RequstCallBackFormComponent,
-    
-    PopularAirlineAndHotelsComponent,
-    
-    DetailComponent,
-    
+    ContactusComponent,    
+    AboutusComponent,    
+    ToursComponent,    
+    DealsComponent,    
+    UmrahComponent,    
+    VisaComponent,    
+    RequstCallBackFormComponent,    
+    PopularAirlineAndHotelsComponent,    
+    DetailComponent,    
     VisaListsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    NoopAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
     CommonModule,
@@ -225,10 +119,10 @@ import { VisaListsComponent } from './components/visa/visa-lists/visa-lists.comp
       separator: '  -  ', 
       format: "DD/MM/YYYY",
     }),
-    NgxGalleryModule
+    NgxGalleryModule,
+    MatTabsModule
   ],
   providers: [MainService, CookieService, DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
->>>>>>> master
