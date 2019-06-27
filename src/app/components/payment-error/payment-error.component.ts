c924e1f9a52bb3ea1a9e16c163b777fda8ae5b0a
mzuhaib <mzuhaib.1@gmail.com>
2019-06-28 00:22:44 +0500

    payment error component updated

--- src/app/components/payment-error/payment-error.component.ts
+++ src/app/components/payment-error/payment-error.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MainService } from 'src/app/services/main.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-payment-error',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentErrorComponent implements OnInit {
 
-  constructor() { }
+  queryParams;
+  refNo;
+  referenceNo:string = '';
+
+  constructor(private __actRouter: ActivatedRoute, private __ms: MainService) { }
 
   ngOnInit() {
+    this.queryParams = this.__actRouter.snapshot.queryParams;
+    this.refNo = this.queryParams.__token;
+    if(this.refNo != ''){
+      let pay_errorUrl = 'http://www.cheapfly.pk/rgtapp/index.php/services/Ticket/paymentError';
+      let pay_errorObj = Object.assign({_refrenceNo:this.refNo});
+      this.__ms.postData(pay_errorUrl, pay_errorObj).subscribe(res => {
+        this.referenceNo = res['ref_no'];
+      })
+    }
   }
 
 }
