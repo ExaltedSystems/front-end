036c32209ea6e4172363dc716be57ad6d5fbaa8b
Abdul Rehman <nadeemkhan@exalted.pk>
2019-07-02 10:28:55 +0500

    Modify Footer and Icons Module

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { IconUsers, IconChevronDown, IconChevronUp, IconMinus, IconPlus, IconCheck, IconMapPin, IconCalendar, IconXCircle, IconSend, 
   IconAward, IconClock, IconPlusCircle, IconArrowRight, IconMinusCircle, IconSliders, IconBell, IconSearch, IconStar, IconMap, IconUser,
-IconGlobe, IconDollarSign, IconPhoneCall, IconMenu, IconFacebook, IconInstagram, IconLinkedin, IconTwitter } from 'angular-feather';
+IconGlobe, IconDollarSign, IconPhoneCall, IconMenu, IconFacebook, IconInstagram, IconLinkedin, IconTwitter, IconTv } from 'angular-feather';
 const icons = [
   IconUsers, IconClock, IconPlusCircle, IconMinusCircle, IconArrowRight, IconChevronDown, IconChevronUp, IconMinus, IconPlus, 
   IconCheck, IconMapPin, IconCalendar, IconXCircle, IconSend, IconAward, IconSliders, IconBell, IconSearch, IconStar, IconMap, IconUser,
-  IconGlobe, IconDollarSign, IconPhoneCall, IconMenu, IconFacebook, IconInstagram, IconLinkedin, IconTwitter
+  IconGlobe, IconDollarSign, IconPhoneCall, IconMenu, IconFacebook, IconInstagram, IconLinkedin, IconTwitter, IconTv
 ]
 @NgModule({
   imports: icons,
